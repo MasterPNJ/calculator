b6a9c35bb0182ea87851662b5638cfcdfd499867
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2025-03-04 09:15:47 +0100

    moyenne
    moyenne

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,6 +7,11 @@ class TestCalculator(TestCase):
 
     def test_sum(self):
         self.assertEqual(self.calc.mysum(1, 2), 3)
+        
+    def test_average(self):
+        self.assertEqual(self.calc.myaverage(2, 4), 3)
+        self.assertEqual(self.calc.myaverage(10, 20), 15)
+        self.assertAlmostEqual(self.calc.myaverage(1, 2), 1.5)
 
     
     
